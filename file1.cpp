3df9b11569896df9cf33ed75139e9650999efe04
Rokibul Islam <rokib.hridoy@gmail.com>
2021-03-13 23:59:12 +0600

    Changed file1.cpp drastically

--- file1.cpp
+++ file1.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 
+void changeTheFile(){
+    std::cout << "This file has been changed drastically" << std::endl;
+}
+
 int main(){
     std::cout << "This is file1" << std::endl;
+    changeTheFile();
 }
